8a423c352bbc750339e42883542f724dfd2cf9da
2017030437 <hdhted02@gmail.com>
2022-04-19 15:33:33 +0900

    2022-04-19 BOJ 문제풀이

---
+++ BOJ/11052. 카드 구매하기.py
@@ -0,0 +1,16 @@
+# 11052. 카드 구매하기
+
+n = int(input())
+
+p = [0] + list(map(int, input().split()))
+
+dp = [0] * (n + 1)
+
+dp[1] = p[1]
+
+for i in range(2, n + 1):
+    for j in range(1, i + 1):
+        if dp[i] < dp[i - j] + p[j]:
+            dp[i] = dp[i - j] + p[j]
+
+print(dp[n])
