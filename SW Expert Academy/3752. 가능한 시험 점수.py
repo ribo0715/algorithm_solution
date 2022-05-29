763dd1494f1817e41b3af7ab4c79d7bc7938c86d
ribo0715 <hdhted02@gmail.com>
2022-05-30 05:18:03 +0900

    2022-05-30 SW 문제풀이

---
+++ SW Expert Academy/3752. 가능한 시험 점수.py
@@ -0,0 +1,48 @@
+# 3752. 가능한 시험 점수
+
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+def calc_possible_case_num(scores, N):
+    q = deque()
+    possible_score = deque() # 현재까지 가능한 점수들을 담음
+
+    index = 0
+    # 첫번째 문제를 맞춘 경우, 못맞춘 경우
+    q.append(0)
+    q.append(scores[index])
+    possible_score.append(0)
+    possible_score.append(scores[index])
+
+    index += 1
+
+    while q and index < N:
+        for _ in range(len(q)):
+            curr_score = q.popleft()
+
+            q.append(curr_score) # index번째 문제를 틀린 경우
+
+            temp_score = curr_score + scores[index] # index번째 문제를 맞춘 경우
+            if temp_score not in possible_score:
+                possible_score.append(temp_score)
+                q.append(temp_score)
+
+
+        index += 1
+
+    return len(possible_score)
+
+
+if __name__ == "__main__":
+    T = int(input())
+
+    for test_case_num in range(1, T + 1):
+        N = int(input())
+        scores = list(map(int, input().split()))
+        scores.sort(reverse=True)
+        answer = calc_possible_case_num(scores, N)
+
+        print("#{} {}".format(test_case_num, answer))
+
