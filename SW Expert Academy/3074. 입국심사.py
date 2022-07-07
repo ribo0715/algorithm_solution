f0ea0b20e59127c9763c52999247ccaf1677c0d7
2017030437 <hdhted02@gmail.com>
2022-07-08 05:35:01 +0900

    2022-07-08 SWEA 문제풀이

---
+++ SW Expert Academy/3074. 입국심사.py
@@ -0,0 +1,21 @@
+# 3074. 입국심사
+"""
+모두 심사를 받기 위해 걸리는 최소한의 시간
+
+"""
+
+if __name__ == "__main__":
+    T = int(input())
+
+    for test_case in range(1, T + 1):
+        N, M = map(int, input().split())
+        tk = []
+        for _ in range(N):
+            cur_tk = int(input())
+            tk.append(cur_tk)
+
+
+
+
+
+        print("#{} {}".format(test_case, answer))
