02e3c7f58e90057ebbc04509e685d2a0a7cb4edd
Seung Hyun Ha <102615647+ribo0715@users.noreply.github.com>
2022-07-23 23:35:02 +0900

    2022-07-23 LC 문제풀이
    2022-07-23 LC 문제풀이

---
+++ Leet Code/1342. Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,17 @@
+# 1342. Number of Steps to Reduce a Number to Zero
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        count = 0
+        
+        while num != 0:
+            if num % 2 == 0:
+                num = num // 2
+            
+            else:
+                num -= 1
+        
+            count += 1
+        
+        return count
+        
