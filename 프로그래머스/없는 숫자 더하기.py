634a91adea7c92811e8a15dbce399e0b3f716ce1
Seung Hyun Ha <102615647+ribo0715@users.noreply.github.com>
2022-08-13 22:35:42 +0900

    2022-08-13 프로그래머스 문제풀이
    2022-08-13 프로그래머스 문제풀이

--- 프로그래머스/없는 숫자 더하기.py
+++ 프로그래머스/없는 숫자 더하기.py
@@ -1,1 +1,12 @@
 # 없는 숫자 더하기
+
+def solution(numbers):
+    # answer = -1
+    temp = set(range(10))
+    # print(temp)
+    
+    for number in numbers:
+        temp.discard(number)
+    
+    answer = sum(temp)
+    return answer
