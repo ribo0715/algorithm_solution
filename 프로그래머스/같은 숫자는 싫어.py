fc51519214c617dbd837203b9ae5d9af7a1394fe
ribo0715 <hdhted02@gmail.com>
2022-08-21 00:34:58 +0900

    2022-08-21 프로그래머스 문제풀이

--- 프로그래머스/같은 숫자는 싫어.py
+++ 프로그래머스/같은 숫자는 싫어.py
@@ -1,5 +1,18 @@
+# 같은 숫자는 싫어
+"""
+연속으로 나타나는 숫자 제거
+"""
+
+
 def solution(arr):
     answer = []
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+
+    prev = arr[0]  # 이전 숫자를 담을 변수
+    answer.append(prev)  # 가장 첫번째 원소는 바로 answer에 넣음
+
+    for cur in arr:
+        if prev != cur:  # 현재 숫자가 이전과 다른 경우
+            answer.append(cur)
+            prev = cur  # 현재 숫자(cur)를 prev에 담음
+
     return answer
