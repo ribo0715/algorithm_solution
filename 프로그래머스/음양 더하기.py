17711be2965f9fea22e4daa08993438517de6f59
ribo0715 <hdhted02@gmail.com>
2022-08-24 23:52:19 +0900

    2022-08-24 프로그래머스 문제풀이

--- 프로그래머스/음양 더하기.py
+++ 프로그래머스/음양 더하기.py
@@ -1,1 +1,17 @@
 # 음양 더하기
+"""
+해당하는 sign이 true이면 양수, false이면 음수
+-> 총 합
+"""
+
+
+def solution(absolutes, signs):
+    answer = 0
+
+    for i in range(len(absolutes)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer += absolutes[i] * (-1)
+
+    return answer
