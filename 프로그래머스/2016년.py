fb3771a1dd4a395a9124c32d0b2604eaed1d57fb
ribo0715 <hdhted02@gmail.com>
2022-09-03 02:54:03 +0900

    2022-09-03 프로그래머스 문제풀이

--- 프로그래머스/2016년.py
+++ 프로그래머스/2016년.py
@@ -1,5 +1,17 @@
 # 2016년
+"""
+1월 1일 금요일 -> a월 b일은 무슨 요일?
+
+몇일 차이인지 계산 -> 7로 나눔
+"""
+
 
 def solution(a, b):
-    answer = ''
+    week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+
+    month_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    total_day = sum(month_days[:a]) + b - 1
+    answer = week[(total_day + 5) % 7]
+
     return answer
