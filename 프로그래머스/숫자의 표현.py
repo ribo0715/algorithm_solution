27326ff78859b1ad942f31b9bb366bdc93dc1179
ribo0715 <hdhted02@gmail.com>
2022-09-25 23:40:04 +0900

    2022-09-25 프로그래머스 문제풀이

--- 프로그래머스/숫자의 표현.py
+++ 프로그래머스/숫자의 표현.py
@@ -1,3 +1,20 @@
+# 숫자의 표현
+"""
+완전탐색 -> 1부터 시작해서 확인
+"""
+
+
 def solution(n):
     answer = 0
+
+    for i in range(1, n + 1):
+        cur_sum = 0
+        for j in range(i, n + 1):
+            cur_sum += j
+            if cur_sum == n:
+                answer += 1
+                break
+            elif cur_sum > n:
+                break
+
     return answer
