16f991a2bdd66649b9243b1961a4613ea252cd74
ribo0715 <hdhted02@gmail.com>
2022-10-11 21:58:49 +0900

    2022-10-11 프로그래머스 문제풀이

--- 프로그래머스/예상 대진표.py
+++ 프로그래머스/예상 대진표.py
@@ -1,7 +1,32 @@
-def solution(n,a,b):
-    answer = 0
+# 예상 대진표
+"""
+이진탐색과 같이 점점 올라가도록함
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+아래로 내려가면서 해도 같지않을까? -> 어짜피 최악의 경우 같은 시간복잡도니까
 
+1~2 / 1~4 / 1~8/ 1~16 / 1~32 / 1~64
+"""
+
+import math
+
+
+def solution(n, a, b):
+    left = 0
+    right = n
+
+    count = math.log2(n)
+
+    while left < right:  # a와 b를 찾으러 갈때 지나가는 노드
+        mid = (left + right) // 2
+
+        if a <= mid and b <= mid:
+            right = mid
+        elif mid < a and mid < b:
+            left = mid + 1
+        else:
+            break
+
+        count -= 1
+
+    answer = count
     return answer
