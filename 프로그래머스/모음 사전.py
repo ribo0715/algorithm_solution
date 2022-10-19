59cae09c3fe7009fc4f90bf511e00635a65b2bdf
ribo0715 <hdhted02@gmail.com>
2022-10-19 21:46:01 +0900

    2022-10-19 프로그래머스 문제풀이

--- 프로그래머스/모음 사전.py
+++ 프로그래머스/모음 사전.py
@@ -1,1 +1,21 @@
+# 모음 사전
+"""
+그냥 다 해봐도 될듯...? 어렵게 계산해서 딱 뽑기엔 너무 머리아프다...
+"""
 
+from itertools import product
+
+
+def solution(word):
+    words = []
+
+    for i in range(1, 6):
+        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
+            temp_word = "".join(list(c))
+            words.append(temp_word)
+
+    words.sort()
+
+    answer = words.index(word) + 1
+
+    return answer
