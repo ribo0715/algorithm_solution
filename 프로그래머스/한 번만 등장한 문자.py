d7df84876c884f2a22f01e8d53a96d0434882dfd
HaSeungHyun <109508635+HaSeungHyun@users.noreply.github.com>
2022-12-01 18:15:34 +0900

    2022-12-01 프로그래머스 문제풀이

--- 프로그래머스/한 번만 등장한 문자.py
+++ 프로그래머스/한 번만 등장한 문자.py
@@ -1,1 +1,21 @@
-
+def solution(s):
+    answer = ''
+    
+    log = []
+    answer_list = []
+    
+    for c in s:
+        if c not in answer_list:
+            answer_list.append(c)
+    
+        else:
+            log.append(c)
+        
+    for target in log:
+        if target in answer_list: 
+            answer_list.remove(target)
+            
+    answer_list.sort()
+    answer = "".join(answer_list)
+    
+    return answer
