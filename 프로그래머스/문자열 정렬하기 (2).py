9480437122240524384341b82ebfa517f4602221
ribo0715 <hdhted02@gmail.com>
2022-12-21 23:20:05 +0900

    2022-12-21 프로그래머스 문제풀이

--- 프로그래머스/문자열 정렬하기 (2).py
+++ 프로그래머스/문자열 정렬하기 (2).py
@@ -1,1 +1,9 @@
+def solution(my_string):
+    answer = ''
 
+    my_string = my_string.lower()
+    my_string_sorted_list = sorted(list(my_string))
+
+    answer = "".join(my_string_sorted_list)
+
+    return answer
