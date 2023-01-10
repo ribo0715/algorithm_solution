7b37c7f6f34e88e247cea1ff21ace9bfa52db824
ribo0715 <102615647+ribo0715@users.noreply.github.com>
2023-01-10 23:37:55 +0900

    2023-01-10 프로그래머스 문제풀이

--- 프로그래머스/저주의 숫자 3.py
+++ 프로그래머스/저주의 숫자 3.py
@@ -1,1 +1,25 @@
+"""
+1부터 100번
+3의 배수, 3이 들어간 수들 제외하고 넣음
+"""
 
+def solution(n):
+    answer = 0
+    
+    answer_list = [0]
+    num = 1 # 1부터 시작
+    for i in range(n):
+        flag = True
+        while flag:
+            if "3" in str(num):
+                num += 1
+            elif num % 3 == 0:
+                num += 1
+            else:
+                answer_list.append(num)
+                flag = False
+                num += 1
+            
+    # print(answer_list)
+    answer = answer_list[n]
+    return answer
